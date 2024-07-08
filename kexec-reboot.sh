0fe812bdcfddde507d3cdc56c8a9c4cb4c390466
sxwesz <3771818810@qq.com>
2024-07-08 18:05:28 +0800

    Renamed files and updated its contents

--- kexec-reboot.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-exec execlineb -s0 $(dirname ${0})/required-root.exec "if { service kexec-load start } reboot"
--- whoami.sh
+++
@@ -1,2 +0,0 @@
-#!/usr/bin/env sh
-exec execlineb -s0 $(dirname ${0})/required-root.exec whoami
