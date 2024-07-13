90ede73916a5da115980646603b71b1a8445bef9
sxwesz <3771818810@qq.com>
2024-07-14 05:48:09 +0800

    profile.d/90environment.sh add de-duplication and check whether PATH is valid

--- 90environment.sh
+++ 90environment.sh
@@ -3,3 +3,8 @@ do
     export $assign 
 done
 unset assign
+
+export PATH=$(for p in $(echo "${PATH}:/opt/bin" | tr : "\n" | sort -u)
+do
+    [ -e "$p" ] && echo "$p"
+done | tr "\n" ":" | sed "s/:$//g")
