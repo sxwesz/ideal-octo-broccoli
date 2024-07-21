eb39953bcf501f417d94a40e3a010736c6c530ab
kat <3771818810@qq.com>
2024-07-22 03:43:04 +0800

    A fish theme but not good for font show command run used time

---
+++ fish/bobthefish_console.fish
@@ -0,0 +1,8 @@
+set -g theme_powerline_fonts no
+set -g theme_show_exit_status yes
+set -g theme_display_jobs_verbose yes
+set -g theme_display_user yes
+# Just for root hide sudo/(doas -s?) after a few by now just hide at is default_user
+#set -g default_user $USER
+set -g theme_display_hostname yes
+set -g theme_newline_prompt "\$"
