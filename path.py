d235117444cff8037f09d44e6bb0ce35a678cdce
sxwesz <3771818810@qq.com>
2025-01-19 16:10:07 +0800

    formated path.py

--- path.py
+++ path.py
@@ -4,26 +4,33 @@
 import os
 from pathlib import Path
 
-_sum = 0
-effect = 0
+def statistics(dirs):
+    _sum = 0
+    effective = 0
 
-count = 0
-
-dirs = os.getenv("PATH").split(os.pathsep)
-for path in dirs:
-    print(path, end=" ")
-    path = Path(path)
-    if not path.exists():
-        print("Not exists")
-        continue
-    for p in path.iterdir():
-        if p.is_file() and os.access(p, os.X_OK):
-            count += 1
-    print(count)
-    _sum += count
     count = 0
-    effect += 1
 
-path_len = len(dirs)
-print("binary:", _sum, "directory:", f"{effect}/{path_len}", "({:.0%})".format(effect / path_len))
+    for path in dirs:
+        print(path, end=" ")
+        path = Path(path)
+        if not path.exists():
+            print("Not exists")
+            continue
+        for p in path.iterdir():
+            if p.is_file() and os.access(p, os.X_OK):
+                count += 1
+        print(count)
+        _sum += count
+        count = 0
+        effective += 1
+    return _sum, effective
+
+def main():
+    dirs = os.getenv("PATH").split(os.pathsep)
+    _sum, effective = statistics(dirs)
+    path_len = len(dirs)
+    print("binary:", _sum, "directory:", f"{effective}/{path_len}", "({:.0%})".format(effective / path_len))
+
+if __name__ == "__main__":
+    main()
 
